5c17db32ba976af6a31445c2fc30afdc96f1ee12
Ali Davut <adavut@gmail.com>
2017-01-31 00:30:45 +0300

    add axios

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './root';
+import axios from 'axios';
+
+axios.defaults.baseURL = process.env.API_URL;
 
 ReactDOM.render(<Root />, document.getElementById('app'));
