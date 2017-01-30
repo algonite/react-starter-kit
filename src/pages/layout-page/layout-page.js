b4e840242e6eaff6635b38d7dd244640cb7f0b6f
Ali Davut <adavut@gmail.com>
2017-01-31 00:01:31 +0300

    config for production builds

--- src/pages/layout-page/layout-page.js
+++ src/pages/layout-page/layout-page.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { Header } from './partials';
 import s from './layout-page.scss';
 
-console.log(s);
-
 export default class LayoutPage extends Component {
   render() {
     const { children } = this.props;
