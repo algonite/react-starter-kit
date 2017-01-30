ce9a0dcf33ab2b197ba17e55ed9057ae503eafb0
Ali Davut <adavut@gmail.com>
2017-01-30 23:49:52 +0300

    add webpack notifier

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -1,5 +1,7 @@
+const WebpackNotifierPlugin = require('webpack-notifier');
 const config = require('../webpack.config');
 
+config.plugins.push(new WebpackNotifierPlugin());
 config.entry.main.push(
   'webpack-dev-server/client?http://localhost:8080'
 )
